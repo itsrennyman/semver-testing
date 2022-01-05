49653df7600a9a4469aff99f27f6a8e2ec6443cf
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-01-05 15:09:56 +0100

    feat: renamed add to add2
    BREAKING CHANGE: add function was renamed to add2, it keeps the same APIs.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   sayHello: function (name) {
     return "Hello! Nice to meet you " + name;
   },
-  add: function (a, b) {
+  add2: function (a, b) {
     return a + b;
   },
   subtract: function (a, b) {
