681ba93d5d3e434f8f30d4a9aeb0d32b0cd4f731
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-01-05 14:49:44 +0100

    feat: added add function \w tests

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ module.exports = {
   sayHello: function (name) {
     return "Hello " + name;
   },
+  add: function (a, b) {
+    return a + b;
+  },
 };
--- index.test.js
+++ index.test.js
@@ -1,5 +1,9 @@
-const { sayHello } = require("./index");
+const { sayHello, add } = require("./index");
 
 it("should say hello", () => {
   expect(sayHello("John")).toBe("Hello John");
+});
+
+it("should sum two numbers", () => {
+  expect(add(1, 2)).toBe(3);
 });
