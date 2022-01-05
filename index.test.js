accfb109182ab1bb3407992c79e520b1a9b91636
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-01-05 15:11:37 +0100

    test: changed test functions

--- index.test.js
+++ index.test.js
@@ -1,9 +1,9 @@
-const { sayHello, add } = require("./index");
+const { sayHello, add2 } = require("./index");
 
 it("should say hello", () => {
   expect(sayHello("John")).toBe("Hello! Nice to meet you John");
 });
 
 it("should sum two numbers", () => {
-  expect(add(1, 2)).toBe(3);
+  expect(add2(1, 2)).toBe(3);
 });
